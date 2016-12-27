9401306fe2c52441108322e374f00563d27f0157
Mike-ThinkGeo <mikeyu@thinkgeo.com>
2016-12-27 19:33:17 +0800

    Upload code.

---
+++ README.md
@@ -0,0 +1,32 @@
+# SqliteSample-ForLinux
+
+### Description
+Working...
+
+![screen](https://raw.githubusercontent.com/ThinkGeo/SqliteSample-ForLinux/master/screenshot.png)
+
+### Requirements
+This sample makes use of the following NuGet Packages.
+* [MapSuite 10.0.0.0]()
+* [ThinkGeo.MapSuite.Layers.SqliteForLinux]()
+
+### Getting Help
+
+[Map Suite Desktop for Winforms Wiki Resources](http://wiki.thinkgeo.com/wiki/map_suite_desktop_edition)
+
+[Map Suite Desktop for Winforms Product Description](http://thinkgeo.com/map-suite-developer-gis/desktop-edition/)
+
+[ThinkGeo Community Site](http://community.thinkgeo.com/)
+
+[ThinkGeo Web Site](http://www.thinkgeo.com)
+
+### Key APIs
+This example makes use of the following APIs:
+
+Working...
+
+### About Map Suite
+Map Suite is a set of powerful development components and services for the .Net Framework.
+
+### About ThinkGeo
+ThinkGeo is a GIS (Geographic Information Systems) company founded in 2004 and located in Frisco, TX. Our clients are in more than 40 industries including agriculture, energy, transportation, government, engineering, software development, and defense.
---
+++ SqliteLayer/MainForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using ThinkGeo.MapSuite;
+using ThinkGeo.MapSuite.Layers;
+using ThinkGeo.MapSuite.WinForms;
+using ThinkGeo.MapSuite.Styles;
+
+namespace ThinkGeo.MapSuite.Samples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            winformsMap1.MapUnit = GeographyUnit.Meter;
+
+            LayerOverlay overlay = new LayerOverlay();
+
+            //To resolve issue that we cannot run the executable by double click it on linux, we need to find out the absolute path by reflection.
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string sqliteFullFileName = Path.GetFullPath(Path.Combine(baseDirectory, "../../App_Data/USStates.sqlite"));
+
+            SqliteFeatureLayer sqliteFeatureLayer = new SqliteFeatureLayer($"Data Source={sqliteFullFileName};Version=3;", "table_name", "id", "geometry");
+			sqliteFeatureLayer.ZoomLevelSet.ZoomLevel01.DefaultAreaStyle = AreaStyles.State1;
+			sqliteFeatureLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+            overlay.Layers.Add(sqliteFeatureLayer);
+
+            winformsMap1.Overlays.Add(overlay);
+
+            sqliteFeatureLayer.Open();
+            winformsMap1.CurrentExtent = sqliteFeatureLayer.GetBoundingBox();
+
+            winformsMap1.Refresh();
+        }
+    }
+}
